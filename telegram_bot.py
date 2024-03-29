d522dfd3c72d2894faf47ee613d5c1b083a7ebdf
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-03-29 19:18:02 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,15 +2,17 @@ import asyncio
 import logging
 import aiogram
 import my_token
-from routers import help,start_end_info, post_info
-
+from routers import help,start_end_info, post_info, calendar
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 #from config_reader import config
+
 
 async def main():
     bot = aiogram.Bot(token=my_token.my_token)
     dp = aiogram.Dispatcher()
-    dp.include_routers(help.router, start_end_info.router, post_info.router)
+    dp.include_routers(help.router, calendar.router, start_end_info.router, post_info.router)
     await bot.delete_webhook(drop_pending_updates=True)
+
     await dp.start_polling(bot)
 
 
