d11855d0ca0671b336047a686191a1ab27ed7339
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2024-03-29 22:22:07 +0300

    Update calendar.py

--- routers/calendar.py
+++ routers/calendar.py
@@ -1,7 +1,9 @@
 import re
 import aiogram
 from aiogram.filters.command import Command, CommandStart
-from aiogram_calendar import SimpleCalendar, SimpleCalendarCallback, DialogCalendar, DialogCalendarCallback, get_user_locale
+#from telegram_calendar import SimpleCalendar, SimpleCalendarCallback, DialogCalendar, DialogCalendarCallback, get_user_locale
+from routers.telegram_calendar import simple_calendar
+from routers.telegram_calendar import common
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, CallbackQuery
 from datetime import datetime
@@ -12,14 +14,14 @@ router = aiogram.Router()
 async def calendar(message: aiogram.types.Message):
     await message.answer(
         "Календарь",
-        reply_markup=await SimpleCalendar(locale=await get_user_locale(message.from_user)).start_calendar()
+        reply_markup=await simple_calendar.SimpleCalendar(locale=await common.get_user_locale(message.from_user)).start_calendar()
     )
 
 
-@router.callback_query(SimpleCalendarCallback.filter())
+@router.callback_query(simple_calendar.SimpleCalendarCallback.filter())
 async def process_simple_calendar(callback_query: CallbackQuery, callback_data: CallbackData):
-    calendar = SimpleCalendar(
-        locale=await get_user_locale(callback_query.from_user), show_alerts=True
+    calendar = simple_calendar.SimpleCalendar(
+        locale=await common.get_user_locale(callback_query.from_user), show_alerts=True
     )
     calendar.set_dates_range(datetime(2000, 1, 1), datetime(2100, 12, 31))
     selected, date = await calendar.process_selection(callback_query, callback_data)
